1ce4ce8c3a17972e924e91e32847cc9d0da046f0
Ishan11032005GitHub <ishan11032005@gmail.com>
2026-04-26 01:24:41 +0530

    fix css path

--- backend/main.py
+++ backend/main.py
@@ -111,7 +111,6 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        # "https://your-vercel-app.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
